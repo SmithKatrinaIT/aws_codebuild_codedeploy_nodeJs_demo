0a282175ae77f9a313e2cc30f431144cb72fa545
ksmith12 <katrina.smith@cox.com>
2024-03-12 14:51:28 -0400

    updated version 3

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('Hello World version 2!')
+  res.send('Hello World version 3!')
 })
 
 app.get('/status', (req, res) => {
